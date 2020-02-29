8270707057ac6a07da43aa29a7869e5ce0404493
Adam Jessop <adamj2109@gmail.com>
2020-02-29 21:07:52 +0000

    Pager setup

--- calendar/calendar/HomeSections/HomeSectionsViewController.swift
+++ calendar/calendar/HomeSections/HomeSectionsViewController.swift
@@ -10,53 +10,60 @@ import UIKit
 
 class HomeSectionsViewController: UIViewController {
     
-    var sections = [TodaySectionViewController(), UpcomingEventsViewController(), CalendarViewController(), SettingsViewController()]
+    let sections = [TodaySectionViewController(), UpcomingEventsViewController(), CalendarViewController(), SettingsViewController()]
     
-    var pagingViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-    
-    var pageControl = UIPageControl()
+    let pagingViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+    let pageControl = UIPageControl()
     
     var index = 0
     
     override func viewDidLoad() {
+        activateSections()
         setupPageControl()
         setupPager()
     }
     
+    private func activateSections() {
+        sections.forEach({ (vc) in
+            let _ = vc.view
+        })
+    }
+        
     private func setupPageControl() {
-        let pageControl = UIPageControl()
         view.addSubview(pageControl)
-        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            pageControl.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             pageControl.widthAnchor.constraint(equalToConstant: 100),
             pageControl.heightAnchor.constraint(equalToConstant: 50),
             pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
         ])
         pageControl.numberOfPages = sections.count
         pageControl.currentPage = 0
-        pageControl.currentPageIndicatorTintColor = .blue
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .darkGray
     }
     
     private func setupPager() {
-//        guard let pagingView = pagingViewController.view else {return}
-//        addChild(pagingViewController)
-//        view.addSubview(pagingView)
-//        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            pagingView.topAnchor.constraint(equalTo: view.topAnchor),
-//            pagingView.bottomAnchor.constraint(equalTo: pageControl.topAnchor, constant: -20),
-//            pagingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            pagingView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-//        ])
-//        pagingViewController.dataSource = self
-//        pagingViewController.delegate = self
-//        if let firstViewController = sections.first {
-//            pagingViewController.setViewControllers([firstViewController],
-//                                                    direction: .forward,
-//                                                    animated: true,
-//                                                    completion: nil)
-//        }
+        guard let pagingView = pagingViewController.view else {return}
+        addChild(pagingViewController)
+        view.addSubview(pagingView)
+        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pagingView.topAnchor.constraint(equalTo: view.topAnchor),
+            pagingView.bottomAnchor.constraint(equalTo: pageControl.topAnchor, constant: -20),
+            pagingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pagingView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        pagingViewController.dataSource = self
+        pagingViewController.delegate = self
+        if let firstViewController = sections.first {
+            pagingViewController.setViewControllers([firstViewController],
+                                                    direction: .forward,
+                                                    animated: true,
+                                                    completion: nil)
+        }
     }
     
 }
@@ -66,6 +73,8 @@ extension HomeSectionsViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         if completed, finished, let currentVC = pageViewController.viewControllers?.last, let currentIndex = sections.firstIndex(of: currentVC) {
             index = currentIndex
+            pageControl.currentPage = index
+            pageControl.updateCurrentPageDisplay()
         }
     }
     
@@ -75,15 +84,11 @@ extension HomeSectionsViewController: UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard index > 0 else {return nil}
-        pageControl.currentPage = index
-        pageControl.updateCurrentPageDisplay()
         return sections[index - 1]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard index + 1 < sections.count else {return nil}
-        pageControl.currentPage = index
-        pageControl.updateCurrentPageDisplay()
         return sections[index + 1]
     }
     
